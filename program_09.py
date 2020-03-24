9b6900ecb11ee06acca0f4c16c1802e646421c33
Ankit Ghanghas <aghangha@scholar-fe04.rcac.purdue.edu>
2020-03-24 18:51:41 -0400

    initial scripts

--- program_09.py
+++ program_09.py
@@ -1,8 +1,15 @@
-#!/bin/env python
-# add your header here
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
 #
+"""
+Created on Friday 2020-03-20
+Author: Ankit Ghanghas
+
+
+"""
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 def ReadData( fileName ):
     """This function takes a filename as input, and returns a dataframe with
@@ -24,14 +31,15 @@ def ReadData( fileName ):
     ReplacedValuesDF = pd.DataFrame(0, index=["1. No Data"], columns=colNames[1:])
      
     return( DataDF, ReplacedValuesDF )
+
  
 def Check01_RemoveNoDataValues( DataDF, ReplacedValuesDF ):
     """This check replaces the defined No Data value with the NumPy NaN value
     so that further analysis does not use the No Data values.  Function returns
     the modified DataFrame and a count of No Data values replaced."""
 
-    # add your code here
-
+    DataDF[DataDF==-999.00]=np.nan
+    ReplacedValuesDF.loc['1. No Data',:]=DataDF.isna().sum()
     return( DataDF, ReplacedValuesDF )
     
 def Check02_GrossErrors( DataDF, ReplacedValuesDF ):
@@ -40,8 +48,11 @@ def Check02_GrossErrors( DataDF, ReplacedValuesDF ):
     DataFrames with data the has passed, and counts of data that have not 
     passed the check."""
  
-    # add your code here
-
+    DataDF.Precip[(DataDF['Precip']>25) | (DataDF['Precip']<0)]=np.nan
+    DataDF['Wind Speed'][(DataDF['Wind Speed']>10) | (DataDF['Wind Speed']<0)]=np.nan
+    DataDF['Max Temp'][(DataDF['Max Temp']>35) | (DataDF['Max Temp']<-25)]=np.nan
+    DataDF['Min Temp'][(DataDF['Min Temp']>35) | (DataDF['Min Temp']<-25)]=np.nan
+    ReplacedValuesDF.loc['2. Gross Error',:]=DataDF.isna().sum()-ReplacedValuesDF.sum()
     return( DataDF, ReplacedValuesDF )
     
 def Check03_TmaxTminSwapped( DataDF, ReplacedValuesDF ):
@@ -49,9 +60,9 @@ def Check03_TmaxTminSwapped( DataDF, ReplacedValuesDF ):
     minimum air temperature, and swaps the values when found.  The function 
     returns modified DataFrames with data that has been fixed, and with counts 
     of how many times the fix has been applied."""
-    
-    # add your code here
-
+    count= len(DataDF.loc[DataDF['Min Temp']>DataDF['Max Temp']])
+    DataDF.loc[DataDF['Min Temp']>DataDF['Max Temp'],['Min Temp','Max Temp']]= DataDF.loc[ DataDF['Min Temp']>DataDF['Max Temp'],['Max Temp','Min Temp']].values 
+    ReplacedValuesDF.loc['3. Swapped',:]=[0,count,count,0]
     return( DataDF, ReplacedValuesDF )
     
 def Check04_TmaxTminRange( DataDF, ReplacedValuesDF ):
@@ -61,8 +72,10 @@ def Check04_TmaxTminRange( DataDF, ReplacedValuesDF ):
     that has been checked, and with counts of how many days of data have been 
     removed through the process."""
     
-    # add your code here
-
+    count= len(DataDF.loc[(DataDF['Max Temp']-DataDF['Min Temp']>25)])
+    DataDF.loc[(DataDF['Max Temp']-DataDF['Min Temp']>25),['Min Temp','Max Temp']]=np.nan
+    ReplacedValuesDF.loc['4. Range', :]=[0,count,count,0]
+ 
     return( DataDF, ReplacedValuesDF )
     
 
@@ -71,9 +84,19 @@ def Check04_TmaxTminRange( DataDF, ReplacedValuesDF ):
 # put the main routines from your code after this conditional check.
 
 if __name__ == '__main__':
+    fig_precip, (ax1,ax2)=plt.subplots(2,sharex=True, sharey= True)
+    fig_wind, (ax11,ax21)=plt.subplots(2,sharex=True, sharey= True)
+    fig_tmax, (ax12,ax22)=plt.subplots(2,sharex=True, sharey= True)
+    fig_tmin, (ax13,ax23)=plt.subplots(2,sharex=True, sharey= True)
 
     fileName = "DataQualityChecking.txt"
     DataDF, ReplacedValuesDF = ReadData(fileName)
+
+    x=DataDF.index
+    l1=ax1.plot(x,DataDF['Precip'],'r')
+    l11=ax11.plot(x,DataDF['Wind Speed'],'r')
+    l12=ax12.plot(x,DataDF['Max Temp'],'r')
+    l13=ax13.plot(x,DataDF['Min Temp'],'r')
     
     print("\nRaw data.....\n", DataDF.describe())
     
@@ -90,6 +113,33 @@ if __name__ == '__main__':
     print("\nCheck for swapped temperatures complete.....\n", DataDF.describe())
     
     DataDF, ReplacedValuesDF = Check04_TmaxTminRange( DataDF, ReplacedValuesDF )
+   
+    x=DataDF.index
+    l2=ax2.plot(x,DataDF['Precip'],'b')
+    fig_precip.suptitle('Precipitaiton')
+    ax2.set_xlabel('Date')
+    fig_precip.legend([l1,l2],labels=['Before Correction', 'After Correction'], loc="upper right")   
+    fig_precip.savefig('precip_plot.png')
+    
+    l21=ax21.plot(x,DataDF['Wind Speed'],'b')
+    fig_wind.suptitle('Wind Speed')
+    ax21.set_xlabel('Date')
+    fig_wind.legend([l11,l21],labels=['Before Correction', 'After Correction'], loc="upper right")   
+    fig_wind.savefig('wind_plot.png')
+    
+    l22=ax22.plot(x,DataDF['Max Temp'],'b')
+    fig_tmax.suptitle('Maximum Temperature')
+    ax22.set_xlabel('Date')
+    fig_tmax.legend([l12,l22],labels=['Before Correction', 'After Correction'], loc="upper right")   
+    fig_tmax.savefig('tmax_plot.png')
+    
+    l23=ax23.plot(x,DataDF['Min Temp'],'b')
+    fig_tmin.suptitle('Minimum Temperature')
+    ax23.set_xlabel('Date')
+    fig_tmin.legend([l13,l23],labels=['Before Correction', 'After Correction'], loc="upper right")   
+    fig_tmin.savefig('tmin_plot.png')
     
     print("\nAll processing finished.....\n", DataDF.describe())
     print("\nFinal changed values counts.....\n", ReplacedValuesDF)
+    DataDF.to_csv('corrected_data.csv',sep='\t')
+    ReplacedValuesDF.to_csv("failed_checks_summar.csv",sep='\t')
